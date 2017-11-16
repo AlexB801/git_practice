9e12a2658b4b2099f7eda1de20c5d2750527f901
Alex Bennett <benedictus.ab@gmail.com>
2017-11-15 19:42:45 -0700

    refactor code

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'colorize'
+require 'git'
+
+class Main
+  include Git
+
+  def self.menu
+    puts "1 Enter git command".colorize(:cyan)
+    puts "2 Exit".colorize(:red)
+    choice = gets.to_i
+    case choice
+    when 1
+      puts "Enter git command"
+      Git.puts_git(gets.strip)
+      menu
+    when 2
+      abort('Goodbye!')
+    else
+      puts "invalid choice".colorize(:yellow)
+      menu
+    end
+  end
+end
+
+Main.menu
